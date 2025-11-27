84a96c22ee84de42be1c4872e2b5ac28ba4429f7
Pablo <pablodeharo872@gmail.com>
2025-11-27 19:15:05 +0100

    Ajuste estructura para deploy en render

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 
 python voltworth/manage.py collectstatic --no-input
 python voltworth/manage.py migrate
